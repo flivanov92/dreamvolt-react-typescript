e1c69718d9a96b8c019f4a30a9d416606f9ff57e
Alex <flivanov92@gmail.com>
2023-01-26 09:59:26 +0000

    create a reusable logo component fix #7

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Image } from './lib/image'
 export { default as Link } from './lib/link';
 export { default as List } from './lib/list';
+export { default as Logo } from './lib/logo';
---
+++ libs/ui/src/lib/logo/logo.tsx
@@ -0,0 +1,10 @@
+import * as S from './logo.style';
+import * as T from './logo.type';
+
+const Logo = ({ imagePath, size, title }: T.Logo) => {
+  return <S.Logo imagePath={imagePath} size={size} title={title} />;
+};
+
+export default Logo;
+
+
